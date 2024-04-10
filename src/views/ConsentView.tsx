fbc73930252fee8df14ec1ec5a6462493a19e839
ssaito-git <67965697+ssaito-git@users.noreply.github.com>
2024-04-10 19:23:14 +0900

    同意画面の修正

--- src/views/ConsentView.tsx
+++ src/views/ConsentView.tsx
@@ -9,15 +9,18 @@ type ConsentProps = {
 export const ConsentView = ({ clientNmae, username, scope }: ConsentProps) => {
   return (
     <MainLayout>
-      <h1>Consent</h1>
-      {clientNmae}
-      {username}
+      <h1>アクセス許可</h1>
+      <div>クライアント名：{clientNmae}</div>
+      <div>ユーザー名：{username}</div>
       {scope && (
-        <ul>
-          {scope.map((value) => (
-            <li>{value}</li>
-          ))}
-        </ul>
+        <div>
+          要求スコープ：
+          <ul>
+            {scope.map((value) => (
+              <li>{value}</li>
+            ))}
+          </ul>
+        </div>
       )}
       <form method="post">
         <button type="submit" name="action" value="cancel">
